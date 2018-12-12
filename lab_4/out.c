0938601756eab91b51b210d39fbd89f7145cd7a4
Tirariharari <gibshman@inbox.ru>
2018-12-12 22:54:57 +0300

    Shared memory

---
+++ lab_4/out.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <time.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+
+#define FILE "file_for_shared_memory"
+
+int main(){
+        /*
+        int file = open (FILE, O_CREAT|O_RDWR|O_TRUNC);
+        if (file <= 0){
+                printf ("Can't create file\n");
+                return 0;
+        }
+
+        write (file, "123456789", 9*sizeof(char));
+        */
+
+        key_t key = ftok(FILE, 29);
+        int smid = shmget (2002, 32, 0666);
+        if (smid == -1)
+        {
+                printf ("Shared memory creation failed\n");
+                return 0;
+        }
+
+        char* addr = shmat (smid, NULL, 0);
+        if (addr == (char*)-1){
+                printf ("Shmat failed\n");
+                return 0;
+        }
+
+        while (1){
+                //time_t tt = time(0);
+                sleep (1);
+		printf("%s", addr);
+        }
+
+        //file = close (file);
+        printf ("%d, %d\n", key, smid);
+        return 0;
+}
